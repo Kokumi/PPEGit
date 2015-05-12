35691b891deb4bfc27d725b508373aa9ce2d3c28
Kokumi <fldebruyck@numericable.fr>
2015-05-12 13:48:02 +0200

    maj fin upload

--- uploadPage.php
+++ uploadPage.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <head>
+    <meta charset="UTF-8">
 	<title>page d'upload de compte-rendus</title>
 	<link rel="stylesheet" href="PPEGit.css">
 </head>
@@ -7,10 +8,42 @@
 <body>
 	<img src="gsb.png" class="img">
 	<h1 class="titre">Ici vous pouvez d&eacute;poser vos comptes-rendus</h1>
+	<p class="warning">Seul les fichier en .pdf sont admis</p>
+        <form action='uploadPage.php' method='post' enctype='multipart/form-data'>
+	 <input type='file' name='fichier'>
+	 <input type='submit' value='Envoyer'>
+	</form>
+        <a href="index.html" class="retour">retour vers l'accueil</a>
 	<?php
-	echo "<form action='traitement.php' method='post' enctype='multipart/form-data'>";
-	echo "<input type='file' name='nom_fichier'>";
-	echo "<input type='submit' value='Envoyer'>";
-	echo "</form>";
+	 
+        if(isset($_FILES['fichier']))
+        {
+        
+            $type=$_FILES['fichier']['name'];
+            $nomComplet=$_FILES['fichier']['tmp_name'];
+            $chemin=pathinfo($type);
+            $ext=$chemin['extension'];
+            $extAutorise=array('pdf');
+            if(in_array($ext, $extAutorise))
+            {
+		print('<br> le fichier final sera'.$type);
+		if(move_uploaded_file($nomComplet, destination))
+		{
+			print("<p class=&quo;reussi&quo;<br>c'est envoyer!");
+		}
+		else
+		{
+			print("<br>Impossible d'envoyer");
+		}
+            }
+            else
+            {
+		echo"<p class=&quot;erreur&quo;><br>le fichier n'est pas en pdf</p>";
+            }
+        }
+        else
+        {
+            print('pas de fichier s&eacute;l&eacute;ctionn&eacute;');
+        }
 	?>
 </body>
